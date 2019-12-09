e7f82227d4b1db268ac2330d21695938f847e3dd
Fridrik <tryggingamidstodin@tmhf.is>
2019-12-09 13:34:26 +0000

    MovieDetails

--- Dr.Cinema/src/components/MovieDetails/index.jsx
+++ Dr.Cinema/src/components/MovieDetails/index.jsx
@@ -1,13 +1,52 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView, Image } from 'react-native';
+import { connect } from 'react-redux';
+import Card from '../common/Card';
+import CardSection from '../common/CardSection';
+import CardSectionSmaller from '../common/CardSectionSmaller';
 
 class MovieDetails extends Component {
+    constructor(props){
+        super(props)
+    }
+
     render(){
+        const { currentMovie } = this.props;
         return(
             <View>
-                <Text>We in</Text>
+                <Card>
+                    <CardSection>
+                        {currentMovie.title}
+                    </CardSection>
+                    <ScrollView>
+                        <CardSectionSmaller>
+                            {currentMovie.year}
+                        </CardSectionSmaller>
+                        <CardSectionSmaller>
+                            {currentMovie.durationMinutes}
+                        </CardSectionSmaller>
+                        <CardSectionSmaller>
+                            <Image
+                                style={{width: 50, height: 50}}
+                                source={{uri: currentMovie.poster}}
+                            />
+                        </CardSectionSmaller>
+                        <CardSectionSmaller>
+                            {currentMovie.plot}
+                        </CardSectionSmaller>
+                    </ScrollView>
+                </Card>
             </View>
         );
     }
 }
-export default MovieDetails;
+
+const mapStateToProps = function(state) {
+    return {
+        token: state.tokenReducer.token,
+        currentCinema: state.currentCinemaReducer.cinema,
+        currentMovie: state.currentMovieReducer.movie
+    }
+}
+
+export default connect(mapStateToProps, null)(MovieDetails);
